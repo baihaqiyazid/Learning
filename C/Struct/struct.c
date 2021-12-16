56013b30cf2bbfb21ae91ce7e6771c792dc7b0c5
baihaqiyazid <89854394+baihaqiyazid@users.noreply.github.com>
2021-12-16 10:12:14 +0700

    Update struct.c

--- C/Struct/struct.c
+++ C/Struct/struct.c
@@ -6,19 +6,20 @@ struct Mahasiswa {
 	int umur;
 	char nim [20];   
 	
-}iman;  
+}iman;  //objek iman 
   
 int main(){  
 	
 	// ========================= CARA 1 =========================
-	struct Mahasiswa rio = {"Rio Mario", 19, "012930123"};
+	struct Mahasiswa rio = {"Rio Mario", 19, "012930123"}; // objek rio
 	
 	// ========================= CARA 2 =========================
 	
 	strcpy(iman.nama, "Iman Jeje");
 	strcpy(iman.nim, "01230123");
 	iman.umur = 20;
-   
+   	
+	// ========================= CETAK ==========================
 	printf("Nama Siswa : %s\n", rio.nama);
 	printf("Umur       : %d\n", rio.umur);
 	printf("NIM        : %s\n", rio.nim);
